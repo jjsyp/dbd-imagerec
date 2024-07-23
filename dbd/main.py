7cab81f8d786f3d506aa81cbd49eead91826d86d
jjsyp <jsypniew@emich.edu>
2024-07-22 21:05:28 -0400

    added daemon threads for threads that werent terminating

--- dbd/main.py
+++ dbd/main.py
@@ -3,28 +3,31 @@ import cProfile
 import time
 
 import tkinter as tk
-from threading import Thread
+from threading import Thread, Event
 
-running = False
+def run_program(stop_event):
+    while not stop_event.is_set():
+        track_loop()  
+        time.sleep(0.001)  
 
-def run_program():
-    try: 
-        while running:
-            track_loop()  # Add your necessary arguments
-            time.sleep(0.0001)  # Or whatever interval you want
-    except KeyboardInterrupt:
-        stop()  # Or any other cleanup code you need
+stop_event = Event()
 
 def start():
-    global running
-    running = True
-    thread = Thread(target=run_program)
+    global thread
+    # Clear the stop event
+    stop_event.clear()
+    # Define and start the thread, set it as a daemon.
+    thread = Thread(target=run_program, args=(stop_event,))
+    thread.daemon = True
     thread.start()
-    
 
 def stop():
-    global running
-    running = False
+    # Set the stop event
+    stop_event.set()
+
+def quit_prog():
+    stop()
+    root.quit()
 
 root = tk.Tk()
 
@@ -33,5 +36,8 @@ start_button.pack()
 
 stop_button = tk.Button(root, text="Stop", command=stop)
 stop_button.pack()
+
+quit_button = tk.Button(root, text="Quit", command=quit_prog)
+quit_button.pack()
 
 root.mainloop()
